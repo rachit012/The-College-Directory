60783344707b1f24e2861f3251e60dfbc3e5010e
rachit012 <rachitthakur012@gmail.com>
2025-12-25 22:31:35 +0530

    added mock data

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import { colleges } from "./data/colleges";
+
 export default function App() {
-  return <h1>College Directory</h1>
+  return (
+    <div className="min-h-screen bg-gray-50 p-6">
+      <div className="max-w-6xl mx-auto">
+        <h1 className="text-2xl font-bold mb-6">
+          College Directory
+        </h1>
+
+        <pre>{JSON.stringify(colleges, null, 2)}</pre>
+      </div>
+    </div>
+  );
 }
---
+++ src/data/colleges.js
@@ -0,0 +1,7 @@
+export const colleges = [
+  { id: 1, name: "IIT Delhi", city: "New Delhi", rating: 4.8, fees: 200000 },
+  { id: 2, name: "Amity University", city: "Noida", rating: 4.2, fees: 300000 },
+  { id: 3, name: "BITS Pilani", city: "Pilani", rating: 4.9, fees: 450000 },
+  { id: 4, name: "DTU", city: "New Delhi", rating: 4.5, fees: 150000 },
+  { id: 5, name: "Manipal Institute", city: "Manipal", rating: 4.4, fees: 350000 }
+];
